cd8ecdf5a44a06b5e4c426d50d9c680a8702ce8d
Ernie3 <ernestgwilsoniii@gmail.com>
2019-01-09 15:49:14 -0500

    fix it for realsies this time

--- src/s3Client.js
+++ src/s3Client.js
@@ -44,7 +44,7 @@ class S3Client {
                     if (err.statusCode === 404) {
                         console.log(
                             "Expected S3 file does not exist! Creating one with a blank JSON as its contents:", 
-                            putParams.Key
+                            this.putParams.Key
                         );
                         console.log("This might be because this is your first time executing the program.");
 
